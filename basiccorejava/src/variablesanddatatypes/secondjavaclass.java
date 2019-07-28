d11ec2c38361fa4984671a55d7af263674d9c0cd
ROHINI <rashigadkari9@gmail.com>
2019-07-28 11:08:46 +0530

    comming arithmetic

---
+++ basiccorejava/src/variablesanddatatypes/secondjavaclass.java
@@ -0,0 +1,19 @@
+package variablesanddatatypes;
+
+public class secondjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// arithmetic operators
+		int a=10;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a%b);
+		
+		
+	}
+
+}
