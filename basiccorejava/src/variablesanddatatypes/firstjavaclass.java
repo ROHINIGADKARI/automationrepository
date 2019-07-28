5c476c43eb326e0fe13dd7d3fa88da50096a9ed2
ROHINI <rashigadkari9@gmail.com>
2019-07-28 11:08:09 +0530

    comming

---
+++ basiccorejava/src/variablesanddatatypes/firstjavaclass.java
@@ -0,0 +1,18 @@
+package variablesanddatatypes;
+
+public class firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         //primitive data types int,char,double,boolean
+		//primitive data types bytes,short,long and float
+		
+		int age=23;//byte,short int,long
+		char gender='F'	;	
+		boolean married =false;
+		double marks=7.8;//float
+		String name="rashi";
+		System.out.println(age);
+	}
+
+}
